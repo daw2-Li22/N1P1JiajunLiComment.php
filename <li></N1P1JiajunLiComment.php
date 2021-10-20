9505649fc1ad59761fffe393f5dd0c99c5564a3b
daw2-Li22 <91276364+daw2-Li22@users.noreply.github.com>
2021-10-20 19:56:42 +0200

    Update N1P1JiajunLiComment.php

--- <li></N1P1JiajunLiComment.php
+++ <li></N1P1JiajunLiComment.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Practica2</title>
+</head>
+<body>
+	<li>Instalar php</li>
+  <li>instalar apache24</li>
+  <li>instalar mysql</li>
+  <li>conectar php a mysql en web</li>
 
+  
+<li>Qualifies the document Li(5)</li>
+	<li>Teacher explanation (5)</li>
+</body>
+</html>
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Secur
